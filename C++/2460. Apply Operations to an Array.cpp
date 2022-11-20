542710f392b6332f8ec7dc361ca1c13dedc1e305
YUVARAJ S <67968287+yuvaraj2selvam@users.noreply.github.com>
2022-11-20 21:58:05 +0530

    Update 2460. Apply Operations to an Array.cpp

--- C++/2460. Apply Operations to an Array.cpp
+++ C++/2460. Apply Operations to an Array.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n=nums.size();
+        int n=nums.size(),z=0;
         vector<int>v;
         for(int i=1;i<n;i++)
-            {
             if(nums[i]==nums[i-1])
                { 
                 nums[i-1]*=2;
-                nums[i]=0;
-                
-               }
-            
-
-
-        }
+                nums[i]=0;}
         
-        int z=0;
-        for(auto x:nums){
+      for(auto x:nums){
             if(x==0)z++;
-            else v.push_back(x);
-        }
+            else v.push_back(x);}
         
-        while(z--)
-            v.push_back(0);
+        while(z--)v.push_back(0);
         
      return v;   
     }
