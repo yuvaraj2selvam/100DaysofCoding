1869866b28d545ea6f48a0831eead156e6effe43
YUVARAJ S <67968287+yuvaraj2selvam@users.noreply.github.com>
2023-01-08 22:27:04 +0530

    Update 485. Max Consecutive Ones.cpp

--- C++/485. Max Consecutive Ones.cpp
+++ C++/485. Max Consecutive Ones.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int ans=-1;
-        int x=1;
-        int n=nums.size();
-        if(n==1)return nums[0];
-        for(int i=1;i<n;i++)
-        {  
-            if(nums[i-1]==nums[i] && nums[i-1]==1)
-                x++;
-            else 
-                x=1;
-            if(nums[i]+nums[i-1]>=1)
-            ans=max(x,ans);
+         int ans=0,c=0;
+          
+        for(auto x:nums)
+        {
+            if(x==1)
+            c+=x;
+            else
+            c=0;
+            ans=max(ans,c);
         }
-        if(ans==-1)return 0;
         return ans;
     }
 };
