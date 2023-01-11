b662cd22af395629dba98139bff45aba8d45a556
YUVARAJ S <67968287+yuvaraj2selvam@users.noreply.github.com>
2023-01-11 20:49:11 +0530

    Create Key Pair.cpp

---
+++ GeeksforGeeks/Key Pair.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    
+	    sort(arr,arr+n);
+	    int i=0,j=n-1;
+	    while(i<j)
+	    {
+	        if(arr[i]+arr[j]==x)return 1;
+	        else if(arr[i]+arr[j]>x)j--;
+	        else i++;
+	    }
+	    return 0;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
