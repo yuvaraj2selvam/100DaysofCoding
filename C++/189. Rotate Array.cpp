229d98a12b8fbf2f88e518d9e286caaf315ebe25
YUVARAJ S <67968287+yuvaraj2selvam@users.noreply.github.com>
2023-01-30 22:29:21 +0530

    Create 189. Rotate Array.cpp

---
+++ C++/189. Rotate Array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        
+        k%=n;
+        
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
